3c53d912cd449614903ae7f0c7b4d560991fbbeb
ZYROUGE <zyrouge@hotmail.com>
2021-05-07 15:10:08 +0530

    Mention prefix support

--- src/events/bot/message.ts
+++ src/events/bot/message.ts
@@ -1,25 +1,36 @@
-import { AppFile } from "@/base/app";
 import Discord from "discord.js";
+import { AppFile } from "@/base/app";
+import { Constants } from "@/util";
 
 const fn: AppFile = (app) => {
     app.bot.on("message", (msg: Discord.Message) => {
-        if (msg.author.bot || !msg.guild) return;
-        
-        const prefix = process.env.PREFIX;
-        if (!prefix || !msg.content.startsWith(prefix)) return;
+        if (msg.author.bot || !msg.guild || !app.bot.user) return;
 
-        const content = msg.content.slice(prefix.length);
+        let prefix: string | undefined, content: string | undefined;
+
+        if (process.env.PREFIX && msg.content.startsWith(process.env.PREFIX)) {
+            prefix = process.env.PREFIX;
+            content = msg.content.slice(prefix.length);
+        }
+
+        const mentionPrefix = Constants.regex.discordMention(app.bot.user.id);
+        if (app.bot.user && mentionPrefix.test(msg.content)) {
+            prefix = `@${app.bot.user.username} `;
+            content = msg.content.replace(mentionPrefix, "").trim();
+        }
+
+        if (!prefix || !content) return;
         const [cmdName, ...args] = content.split(" ");
 
         const command = app.commands.resolve(cmdName);
         if (!command) return;
-    
+
         command.run({
             msg,
             prefix,
-            args
+            args,
         });
     });
-}
+};
 
 export default fn;
--- src/util.ts
+++ src/util.ts
@@ -54,4 +54,8 @@ export const Constants = {
         UserAgent:
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36",
     },
+    regex: {
+        discordMention: (id: string = "\\d+", flags?: string) =>
+            new RegExp(`<@!?${id}>`, flags),
+    },
 };
